7dd7c16cd1a39cf99943996d409981745ce485b5
Beck TJ <bakytbek.tatibekov@gmail.com>
2018-03-29 04:24:28 -0400

    server file is updated

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-const http = require("http");
+const http = require("http"); // BUILT-IN HTTP MODULE (1)
 const express = require("express");
 const app = express();
 // const app = require("./app");
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(app);
+const server = http.createServer(app); //CREATE A SERVER (2)
 
 const productEndpoints = require("./api/routes/products");
 
 app.use("/products", productEndpoints);
+
+// THIS WILL SHOW UP ON LOCALHOST:3000 (_DEFAULT_)
 app.use((req, res) => {
   res.status(200).json({
     message: "this is a test!"
   });
 });
+
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+// var http = require('http');
+
+// var server = http.createServer(function(req, res) {
+// res.writeHead(200);
+// res.end('Hi everybody!');
+// });
+// server.listen(8080);
